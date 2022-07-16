140aba713c58f38801067ed6c42a7e7c9d42d49d
JFH <20402845+jfhenon@users.noreply.github.com>
2022-07-16 17:14:13 +0200

    use newer hydrateRoot

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,53 +1,45 @@
-import React from 'react';
-import { NavLink as Link, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { NavLink as Link, Routes, Route } from "react-router-dom";
 
 // import child components
-import { Counter } from '../counter';
-import { Post } from '../post';
+import { Counter } from "../counter";
+import { Post } from "../post";
 
 // export entry application component
-export class App extends React.Component {
-    constructor() {
-        console.log( 'App.constructor()' );
-        super();
-    }
+export const App = () => {
+  console.log("App()");
 
-    // render view
-    render() {
-        console.log( 'App.render()' );
+  return (
+    <div className="ui-app">
+      {/* navigation */}
+      <div className="ui-app__navigation">
+        <Link
+          className={({ isActive }) =>
+            "nav-ui-app__navigation__link" +
+            (isActive ? " ui-app__navigation__link--active" : "")
+          }
+          to="/"
+          end
+        >
+          Counter
+        </Link>
 
-        return (
-            <div className='ui-app'>
-                {/* navigation */}
-                <div className='ui-app__navigation'>
-                    <Link
-                        className={({ isActive }) => "nav-ui-app__navigation__link" + (isActive ? " ui-app__navigation__link--active" : "")}
-                        to='/'
-                        end
-                    >Counter</Link>
+        <Link
+          className={({ isActive }) =>
+            "ui-app__navigation__link" +
+            (isActive ? " ui-app__navigation__link--active" : "")
+          }
+          to="/post"
+          end
+        >
+          Post
+        </Link>
+      </div>
 
-                    <Link
-                        className={({ isActive }) => "ui-app__navigation__link" + (isActive ? " ui-app__navigation__link--active" : "")}
-                        to='/post'
-                        end
-                    >Post</Link>
-                </div>
-
-                <Routes>
-                    <Route
-                        path='/'
-                        exact
-                        element={ <Counter name='Monica Geller'/> }
-                    />
-
-                    <Route
-                        path='/post'
-                        exact={ true }
-                        element={ <Post /> }
-                    />
-                </Routes>
-                
-            </div>
-        );
-    }
-}
+      <Routes>
+        <Route path="/" end element={<Counter name="Monica Geller" />} />
+        <Route path="/post" end element={<Post />} />
+      </Routes>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 // import App components
 import { App } from './components/app';
 
 // compile App component in `#app` HTML element
-ReactDOM.hydrate( <BrowserRouter><App/></BrowserRouter>, document.getElementById( 'app' ) );
+ReactDOM.hydrateRoot( document.getElementById( 'app' ) , <BrowserRouter><App/></BrowserRouter>);
